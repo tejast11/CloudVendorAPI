7f16ba9c999d0432cf0d42c3fe9a0adda8fe6ae4
Tejas Thakur <tejasthakur1103@gmail.com>
2025-08-28 12:12:37 +0530

    service insatnce used

--- src/main/java/com/tejast11/rest_start/controller/CloudVendorController.java
+++ src/main/java/com/tejast11/rest_start/controller/CloudVendorController.java
@@ -1,37 +1,49 @@
 package com.tejast11.rest_start.controller;
 
 import com.tejast11.rest_start.model.CloudVendor;
+import com.tejast11.rest_start.service.CloudVendorService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cloudVendor")
 public class CloudVendorController {
     CloudVendor cloudVendor;
+    CloudVendorService cloudVendorService;
+
+    public CloudVendorController(CloudVendorService cloudVendorService) {
+        this.cloudVendorService = cloudVendorService;
+    }
 
     //Read
     @GetMapping("{vendorId}")
-    public CloudVendor getCloudVendorDetails(String vendorId){
-        return cloudVendor;
+    public CloudVendor getCloudVendorDetails(@PathVariable("vendorId") String vendorId){
+        return cloudVendorService.getCloudVendor(vendorId);
+    }
+    @GetMapping()
+    public List<CloudVendor> getAllCloudVendorDetails(){
+        return cloudVendorService.getAllCloudVendors();
     }
 
     // Create
     @PostMapping
     public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
-        this.cloudVendor = cloudVendor;
+        cloudVendorService.createCloudVendor(cloudVendor);
         return "Cloud Vendor Created Successfully ";
     }
 
     //Update
     @PutMapping
     public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
-        this.cloudVendor = cloudVendor;
+        cloudVendorService.updateCloudVendor(cloudVendor);
         return "Cloud Vendor Updated Successfully ";
     }
 
     // Delete
     @DeleteMapping("{vendorId}")
-    public String deleteCloudVendorDetails(String vendorId){
-        this.cloudVendor = null;
+    public String deleteCloudVendorDetails(@PathVariable String vendorId){
+        cloudVendorService.deleteCloudVendor(vendorId);
         return "Cloud Vendor Deleted Successfully ";
     }
 }
